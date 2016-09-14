bc0225a2190b35731d37c8ede3b68b03d8748342
Amanda Brown <amanda.brown@gmail.com>
2016-09-14 16:24:23 -0400

    methods problem complete

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slide5Problem
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] example = { 20, 10, 5, 2 };
+            Console.WriteLine("The sum of of the example array is {0}", Sum(example));
+        }
+
+        static int Sum(int [] example)
+        {
+            int placeHolder = 0;
+            foreach(int number in example)
+            {
+                placeHolder += number;
+            }
+            return placeHolder;
+        }
+    }
+}
